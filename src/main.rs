fdc40315376c8765468feb7edf63a76d30af3825
bluelhf <ilari.suhonen@gmail.com>
2023-05-09 21:40:38 +0300

    feat: separate tls into feature

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod custom_headers;
 mod model;
 mod error;
 
-use std::{net::SocketAddr, path::PathBuf};
+use std::net::SocketAddr;
 use uuid::Uuid;
 
 use axum::{
@@ -26,7 +26,10 @@ use axum::http::{HeaderName};
 use axum::http::header::CONTENT_LENGTH;
 use axum::response::{IntoResponse};
 
-use axum_server::tls_rustls::RustlsConfig;
+#[cfg(feature = "tls")] use axum_server::tls_rustls::RustlsConfig;
+#[cfg(feature = "tls")] use std::path::PathBuf;
+
+
 use tower_http::cors::{CorsLayer, Any};
 
 use crate::{
@@ -39,10 +42,6 @@ use crate::{
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let config = RustlsConfig::from_pem_file(
-        [env!("CARGO_MANIFEST_DIR"), "tls", "cert.pem"].iter().collect::<PathBuf>(),
-        [env!("CARGO_MANIFEST_DIR"), "tls", "key.pem"].iter().collect::<PathBuf>()
-    ).await.expect("tls/cert.pem and tls/key.pem should contain relevant tls certificate data");
 
     // Model lives for the lifetime of the program — it is 'effectively static' so fine to leak
     let model: &'static Model = Box::leak(Box::new(Model::with_storage("files".into())));
@@ -53,10 +52,29 @@ async fn main() {
         .layer(cors_layer())
         .with_state(model);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-    axum_server::bind_rustls(addr, config)
-        .serve(app.into_make_service())
-        .await.expect("failed to bind to socket — is something else already there?");
+    let port = std::env::var("PORT")
+        .map(|s| s.parse().expect(&format!("{s} is not a valid port")))
+        .unwrap_or_else(|_| 8080);
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+
+    #[cfg(feature = "tls")]
+    {
+        let config = RustlsConfig::from_pem_file(
+            ["tls", "cert.pem"].iter().collect::<PathBuf>(),
+            ["tls", "key.pem"].iter().collect::<PathBuf>()
+        ).await.expect("tls/cert.pem and tls/key.pem should contain relevant tls certificate data");
+
+        axum_server::bind_rustls(addr, config)
+            .serve(app.into_make_service())
+            .await.expect("failed to bind to socket — is something else already there?");
+    }
+    #[cfg(not(feature = "tls"))]
+    {
+        axum_server::bind(addr)
+            .serve(app.into_make_service())
+            .await.expect("failed to bind to socket — is something else already there?");
+    }
 }
 
 fn cors_layer() -> CorsLayer {
