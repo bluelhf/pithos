b0734c37200ab4bb14b1c7b125069662fe7014f8
bluelhf <ilari.suhonen@gmail.com>
2023-05-10 12:38:15 +0300

    chore(error): remove post- and prefixes

--- src/error.rs
+++ src/error.rs
@@ -11,25 +11,25 @@ use tracing::error;
 
 #[derive(Debug)]
 pub enum PilviError {
-    FileSystemError(io::Error),
-    NoSuchFileError(io::Error),
-    FileCorruptedError(CorruptionError),
-    ContentReadError(axum::Error),
+    FileSystem(io::Error),
+    NoSuchFile(io::Error),
+    FileCorrupted(CorruptionError),
+    ContentRead(axum::Error),
 }
 
 #[derive(Debug)]
 pub enum CorruptionError {
-    InvalidHeader(InvalidHeaderValue),
-    InvalidUri(InvalidUri),
-    InvalidFileName(FromUtf8Error),
+    Header(InvalidHeaderValue),
+    Uri(InvalidUri),
+    FileName(FromUtf8Error),
 }
 
 impl Display for CorruptionError {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            CorruptionError::InvalidHeader(_) => { write!(f, "The server tried to send invalid data.") }
-            CorruptionError::InvalidUri(_) => { write!(f, "Google Cloud Storage returned an invalid object URI.") }
-            CorruptionError::InvalidFileName(_) => { write!(f, "The requested file has been corrupted.") }
+            CorruptionError::Header(_) => { write!(f, "The server tried to send invalid data.") }
+            CorruptionError::Uri(_) => { write!(f, "Google Cloud Storage returned an invalid object URI.") }
+            CorruptionError::FileName(_) => { write!(f, "The requested file has been corrupted.") }
         }
     }
 }
@@ -37,9 +37,9 @@ impl Display for CorruptionError {
 impl Error for CorruptionError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            CorruptionError::InvalidHeader(e) => Some(e),
-            CorruptionError::InvalidUri(e) => Some(e),
-            CorruptionError::InvalidFileName(e) => Some(e)
+            CorruptionError::Header(e) => Some(e),
+            CorruptionError::Uri(e) => Some(e),
+            CorruptionError::FileName(e) => Some(e)
         }
     }
 }
@@ -47,10 +47,10 @@ impl Error for CorruptionError {
 impl PilviError {
     pub fn status_code(&self) -> StatusCode {
         match self {
-            PilviError::FileSystemError(_) => { StatusCode::INTERNAL_SERVER_ERROR }
-            PilviError::FileCorruptedError(_) => { StatusCode::INTERNAL_SERVER_ERROR }
-            PilviError::ContentReadError(_) => { StatusCode::BAD_REQUEST }
-            PilviError::NoSuchFileError(_) => { StatusCode::NOT_FOUND }
+            PilviError::FileSystem(_) => { StatusCode::INTERNAL_SERVER_ERROR }
+            PilviError::FileCorrupted(_) => { StatusCode::INTERNAL_SERVER_ERROR }
+            PilviError::ContentRead(_) => { StatusCode::BAD_REQUEST }
+            PilviError::NoSuchFile(_) => { StatusCode::NOT_FOUND }
         }
     }
 }
@@ -61,10 +61,10 @@ impl Display for PilviError {
         //               actually log properly with axum
         error!("{:?}", self);
         match self {
-            PilviError::FileSystemError(_) => { write!(f, "There was an unexpected file system error while storing your file in the cloud.") }
-            PilviError::ContentReadError(_) => { write!(f, "There was an error transmitting your file over the internet.") }
-            PilviError::FileCorruptedError(_) => { write!(f, "The requested file was corrupted on the server and can't be retrieved.") }
-            PilviError::NoSuchFileError(_) => { write!(f, "The requested file does not exist.") }
+            PilviError::FileSystem(_) => { write!(f, "There was an unexpected file system error while storing your file in the cloud.") }
+            PilviError::ContentRead(_) => { write!(f, "There was an error transmitting your file over the internet.") }
+            PilviError::FileCorrupted(_) => { write!(f, "The requested file was corrupted on the server and can't be retrieved.") }
+            PilviError::NoSuchFile(_) => { write!(f, "The requested file does not exist.") }
         }
     }
 }
@@ -72,10 +72,10 @@ impl Display for PilviError {
 impl Error for PilviError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
         match self {
-            PilviError::FileSystemError(e) => Some(e),
-            PilviError::ContentReadError(e) => Some(e),
-            PilviError::FileCorruptedError(e) => Some(e),
-            PilviError::NoSuchFileError(e) => Some(e)
+            PilviError::FileSystem(e) => Some(e),
+            PilviError::ContentRead(e) => Some(e),
+            PilviError::FileCorrupted(e) => Some(e),
+            PilviError::NoSuchFile(e) => Some(e)
         }
     }
 }
@@ -83,46 +83,46 @@ impl Error for PilviError {
 impl From<io::Error> for PilviError {
     fn from(e: io::Error) -> Self {
         match e.kind() {
-            io::ErrorKind::NotFound => PilviError::NoSuchFileError(e),
-            _ => PilviError::FileSystemError(e)
+            io::ErrorKind::NotFound => PilviError::NoSuchFile(e),
+            _ => PilviError::FileSystem(e)
         }
     }
 }
 
 impl From<axum::Error> for PilviError {
     fn from(e: axum::Error) -> Self {
-        PilviError::ContentReadError(e)
+        PilviError::ContentRead(e)
     }
 }
 
 impl From<hyper::Error> for PilviError {
     fn from(e: hyper::Error) -> Self {
-        PilviError::FileSystemError(io::Error::new(io::ErrorKind::Other, e))
+        PilviError::FileSystem(io::Error::new(io::ErrorKind::Other, e))
     }
 }
 
 
 impl From<FromUtf8Error> for PilviError {
     fn from(e: FromUtf8Error) -> Self {
-        PilviError::FileCorruptedError(CorruptionError::InvalidFileName(e))
+        PilviError::FileCorrupted(CorruptionError::FileName(e))
     }
 }
 
 impl From<cloud_storage::Error> for PilviError {
     fn from(e: cloud_storage::Error) -> Self {
-        PilviError::FileSystemError(io::Error::new(io::ErrorKind::Other, e))
+        PilviError::FileSystem(io::Error::new(io::ErrorKind::Other, e))
     }
 }
 
 impl From<InvalidUri> for PilviError {
     fn from(e: InvalidUri) -> Self {
-        PilviError::FileCorruptedError(CorruptionError::InvalidUri(e))
+        PilviError::FileCorrupted(CorruptionError::Uri(e))
     }
 }
 
 impl From<InvalidHeaderValue> for PilviError {
     fn from(e: InvalidHeaderValue) -> Self {
-        PilviError::FileCorruptedError(CorruptionError::InvalidHeader(e))
+        PilviError::FileCorrupted(CorruptionError::Header(e))
     }
 }
 
